f6e679ffda019bfb9f062cfb9d838330e5a70956
thmspry <thomas.peray5@gmail.com>
2025-11-17 18:37:28 +0100

    Base projet React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <div style={{ padding: '10px', width: '200px' }}>
+            <h1>Hello React 🚀</h1>
+            <button onClick={() => alert("Popup React active !")}>
+                Clique-moi
+            </button>
+        </div>
+    );
+}
+
+export default App;
