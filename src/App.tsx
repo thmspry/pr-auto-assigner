56d7573b0fae723c47b51013f0fb7df77d201e53
thmspry <thomas.peray5@gmail.com>
2025-11-18 19:48:11 +0100

    Card pour les liste, switch à la création

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,55 @@ import {useChromeStorage} from "./hooks/useChromeStorage.ts";
 import type {StorageData} from "./types/storage-data.ts";
 import type {List} from "./types/list.ts";
 import {ListCard} from "./component/ListCard.tsx";
+import {useState} from "react";
 
-const defaultData: StorageData = { lists: [{
-    name: "aaa", people: ['lala', 'lolo']
-    }] };
+const defaultData: StorageData = {
+    lists: [{
+        name: "aaa", people: ['lala', 'lolo']
+    }]
+};
+
+type DisplayModeProps = {
+    isEditMode: boolean,
+    data: StorageData,
+    addList: () => void
+}
+
+function DisplayMode({isEditMode, data, addList}: DisplayModeProps) {
+    if (isEditMode) {
+        return <p>Salut</p>;
+    }
+    return <>
+        <header>
+            <h1>Pull Request Auto Assigner</h1>
+        </header>
+
+        <main>
+            {data.lists.map((list, i) => (
+                <ListCard key={i} name={list.name} people={list.people}/>
+            ))}
+        </main>
+
+        <footer>
+            <button onClick={addList}>Ajouter une liste</button>
+        </footer>
+    </>;
+}
 
 function App() {
     const [data, setData] = useChromeStorage<StorageData>("personLists", defaultData);
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
 
     const addList = () => {
-        const newList: List = { name: "Nouvelle liste", people: [] };
-        setData({ lists: [...data.lists, newList] });
+        setIsEditMode(true);
+        const newList: List = {name: "Nouvelle liste", people: []};
+        setData({lists: [...data.lists, newList]});
     };
 
     return (
-        <div>
-            <button onClick={addList}>Ajouter une liste</button>
-
-            {data.lists.map((list, i) => (
-                <ListCard key={i} name={list.name} people={list.people} />
-            ))}
-        </div>
+        <>
+            <DisplayMode isEditMode={isEditMode} data={data} addList={addList}/>
+        </>
     );
 }
 
--- src/component/ListCard.tsx
+++ src/component/ListCard.tsx
@@ -1,10 +1,11 @@
+import './ListCard.css'
 import React from "react";
 import type {List} from "../types/list.ts";
 
 
 export const ListCard: React.FC<List> = ({ name, people }) => {
     return (
-        <div>
+        <div className={'card'}>
             <h3>{name}</h3>
             <ul>
                 {people.map((person, index) => (
